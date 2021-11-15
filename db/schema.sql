e2788189ed0028d8e8ae4367297218e8e3ddb959
kaohzina <zinanshin.kaoher@gmail.com>
2021-11-15 11:23:31 -0600

    schema from module

--- db/schema.sql
+++ db/schema.sql
@@ -1,40 +1,24 @@
-DROP TABLE IF EXISTS votes;
-DROP TABLE IF EXISTS candidates;
-DROP TABLE IF EXISTS parties;
-DROP TABLE IF EXISTS voters;
+DROP TABLE IF EXISTS employees;
 
-CREATE TABLE parties (
-  id INTEGER AUTO_INCREMENT PRIMARY KEY,
-  name VARCHAR(50) NOT NULL,
-  description TEXT
+CREATE TABLE departments (
+    department_id INTEGER AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    department_name VARCHAR(30) NOT NULL
 );
 
-CREATE TABLE candidates (
-  id INTEGER AUTO_INCREMENT PRIMARY KEY,
-  first_name VARCHAR(30) NOT NULL,
-  last_name VARCHAR(30) NOT NULL,
-  party_id INTEGER,
-  industry_connected BOOLEAN NOT NULL,
-  CONSTRAINT fk_party
-    FOREIGN KEY (party_id)
-    REFERENCES parties(id)
-    ON DELETE SET NULL
+CREATE TABLE roles (
+    role_id INTEGER AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    role_title VARCHAR(50) NOT NULL,
+    salary DECIMAL (10,2) NOT NULL,
+    department_id INTEGER,
+    FOREIGN KEY (department_id) REFERENCES departments(department_id)
 );
 
-CREATE TABLE voters (
-  id INTEGER AUTO_INCREMENT PRIMARY KEY,
-  first_name VARCHAR(30) NOT NULL,
-  last_name VARCHAR(30) NOT NULL,
-  email VARCHAR(50) NOT NULL,
-  created_at DATETIME DEFAULT CURRENT_TIMESTAMP
-);
-
-CREATE TABLE votes (
-  id INTEGER AUTO_INCREMENT PRIMARY KEY,
-  voter_id INTEGER NOT NULL,
-  candidate_id INTEGER NOT NULL,
-  created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
-  CONSTRAINT uc_voter UNIQUE (voter_id),
-  CONSTRAINT fk_voter FOREIGN KEY (voter_id) REFERENCES voters(id) ON DELETE CASCADE,
-  CONSTRAINT fk_candidate FOREIGN KEY (candidate_id) REFERENCES candidates(id) ON DELETE CASCADE
+CREATE TABLE employees (
+    employee_id INTEGER AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    first_name VARCHAR(30) NOT NULL,
+    last_Name VARCHAR(30) NOT NULL,
+    role_title_id INTEGER,
+    FOREIGN KEY (role_title_id) REFERENCES roles(role_id),
+    manager_id INTEGER,
+    FOREIGN KEY (manager_id) REFERENCES employees(employee_id) ON DELETE SET NULL
 );
